765685ee8526f21ec59df99cec4f64e9a5e250bb
martin.ferreyra <martin.ferreyra@proveedor.ues21.edu.ar>
2024-11-13 20:46:05 -0300

    se modifica conexion con github

--- clase_00/src/main/java/com/ejercitacion00/Clase01.java
+++ clase_00/src/main/java/com/ejercitacion00/Clase01.java
@@ -2,9 +2,10 @@ package com.ejercitacion00;
 
 public class Clase01 {
 
-	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-		
-	}
+  public static void main(String[] args) {
+
+    System.out.println("Hola Mundo con github");
+
+  }
 
 }
