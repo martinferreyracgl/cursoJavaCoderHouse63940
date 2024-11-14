2498757c019e56bcf080a06ae904ff49dbc43d6d
martin.ferreyra <martin.ferreyra@proveedor.ues21.edu.ar>
2024-11-14 11:51:18 -0300

    se agrega la clase 02

---
+++ clase_00/src/main/java/com/ejercitacion02/Clase02.java
@@ -0,0 +1,29 @@
+package com.ejercitacion02;
+
+/**
+ * @author bizit
+ *
+ */
+public class Clase02 {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    // Declaración de variables
+    String ver = "Hola Coders surfeando en Java clase 02";
+    int v = 0;
+
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
+        System.out.println("Numero: " + i + " es PAR");
+      } else {
+        System.out.println("Numero: " + i + " es IMPAR");
+      }
+
+    }
+
+  }
+
+}
