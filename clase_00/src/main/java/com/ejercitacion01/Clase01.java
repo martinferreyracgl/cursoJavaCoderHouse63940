e7c94f510888f001d514afa5afe2fe905e97d11e
martin.ferreyra <martin.ferreyra@proveedor.ues21.edu.ar>
2024-11-14 11:52:45 -0300

    se agrega branch

--- clase_00/src/main/java/com/ejercitacion01/Clase01.java
+++ clase_00/src/main/java/com/ejercitacion01/Clase01.java
@@ -4,7 +4,7 @@ public class Clase01 {
 
   public static void main(String[] args) {
 
-    System.out.println("Hola Mundo con github");
+    System.out.println("Hola Mundo con github branch");
 
   }
 
